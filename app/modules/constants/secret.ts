641374d431aa45fdc6fba39d4680e34a0ed40179
Kontam <tehayan0111@gmail.com>
2021-01-17 19:27:58 +0900

    fix: fix invalid format.

--- app/modules/constants/secret.ts
+++ app/modules/constants/secret.ts
@@ -1,12 +1,13 @@
-/* 
+/*
  * File for secret variables
  * Empty values are prepared for CI
+ * Don't commit actual values!!!
  */
 export const secret = {
   SLACK_CLIENT_ID: '',
   SLACK_REDIRECT_URL: '',
 
-  DEV_SCHEME_URL: '' // dev build cannot get tokens from browser,
+  DEV_SCHEME_URL: '', // dev build cannot get tokens from browser,
 
   STORAGE_AUTH_TOKEN: '',
   STORAGE_AUTH_BOT_TOKEN: '',
