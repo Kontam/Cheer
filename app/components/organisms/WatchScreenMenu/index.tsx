1580649130e44aee3930dfaa2d853459b209853a
Kontam <tehayan0111@gmail.com>
2021-03-19 00:45:35 +0900

    feat: styling

--- app/components/organisms/WatchScreenMenu/index.tsx
+++ app/components/organisms/WatchScreenMenu/index.tsx
@@ -2,12 +2,16 @@ import React from 'react';
 import styled from 'styled-components';
 import ScreenMenu from '../ScreenMenu';
 import { useWatchScreenMenu } from './useWatchScreenMenu';
+import buttonIcon from '../../../static/image/megaphoneIcon.svg';
+import { styleConst } from '../../../modules/styles/styleConst';
 
 const WatchScreenMenu: React.FC = () => {
   const { menus, handleMenuButtonClick } = useWatchScreenMenu();
   return (
     <Container>
-      <Button onClick={handleMenuButtonClick} />
+      <Button onClick={handleMenuButtonClick}>
+        <Img src={buttonIcon} alt="menu" />
+      </Button>
       <ScreenMenu menus={menus} />
     </Container>
   );
@@ -15,6 +19,25 @@ const WatchScreenMenu: React.FC = () => {
 
 const Container = styled.div``;
 
-const Button = styled.button``;
+const Button = styled.button`
+  background-color: ${styleConst.basicWhite};
+  border: 3px solid ${styleConst.basicPink};
+  border-radius: 50%;
+  width: 40px;
+  height: 40px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  outline: 0;
+  cursor: pointer;
+
+  :hover {
+    background-color: ${styleConst.lightGreen};
+  }
+`;
+
+const Img = styled.img`
+  width: 30px;
+`;
 
 export default WatchScreenMenu;
