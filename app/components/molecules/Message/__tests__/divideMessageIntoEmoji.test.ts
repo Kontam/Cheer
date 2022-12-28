df786ded89007e9907394f916933faa8b540f121
Kontam <tehayan0111@gmail.com>
2022-12-28 15:07:21 +0900

    refactor: add divideMessageIntoEmoji function

---
+++ app/components/molecules/Message/__tests__/divideMessageIntoEmoji.test.ts
@@ -0,0 +1,49 @@
+import assert from 'power-assert';
+import { divideMessageIntoEmoji } from '../divideMessageIntoEmoji';
+
+describe('SlackのメッセージからEmoji表現を抽出し、登場ごとにメッセージを分割する。', () => {
+  let message: string;
+  describe('メッセージ内にEmoji表現が存在しない時', () => {
+    beforeEach(() => {
+      message = 'Emojiを持たないメッセージ';
+    });
+    test('メッセージをそのまま返し、Emojiリストは空を返す', () => {
+      assert.deepStrictEqual(divideMessageIntoEmoji(message), [[message], []]);
+    });
+  });
+  describe('メッセージ内にEmoji表現1つ存在する時', () => {
+    describe('文中にEmoji表現が存在する時', () => {
+      beforeEach(() => {
+        message = 'Emojiを含んだ:emoji:メッセージ';
+      });
+      test('Emoji前、Emoji後に分割したメッセージリストと、文中のEmojiが入ったリストを返す', () => {
+        assert.deepStrictEqual(divideMessageIntoEmoji(message), [
+          ['Emojiを含んだ', 'メッセージ'],
+          [':emoji:'],
+        ]);
+      });
+    });
+    describe('文頭にEmoji表現が存在する時', () => {
+      beforeEach(() => {
+        message = ':emoji:先頭にEmojiを含んだメッセージ';
+      });
+      test('Emoji前として空文字列、Emoji後に分割したメッセージリストと、文中のEmojiが入ったリストを返す', () => {
+        assert.deepStrictEqual(divideMessageIntoEmoji(message), [
+          ['', '先頭にEmojiを含んだメッセージ'],
+          [':emoji:'],
+        ]);
+      });
+    });
+  });
+  describe('メッセージ内にEmoji表現2つ存在する時', () => {
+    beforeEach(() => {
+      message = 'Emojiを:first:複数:second:含んだメッセージ';
+    });
+    test('Emoji登場ごとに分割したメッセージリストと、文中のEmojiが入ったリストを返す', () => {
+      assert.deepStrictEqual(divideMessageIntoEmoji(message), [
+        ['Emojiを', '複数', '含んだメッセージ'],
+        [':first:', ':second:'],
+      ]);
+    });
+  });
+});
---
+++ app/components/molecules/Message/divideMessageIntoEmoji.ts
@@ -0,0 +1,25 @@
+export type DividedMessageEmoji = [string[], string[]];
+
+export function divideMessageIntoEmoji(message: string) {
+  const slackEmojiRegex = /:[^:]*?:/g;
+  const match = message.match(slackEmojiRegex);
+  if (!match?.length) return [[message], []];
+
+  const separatedMessage = match.reduce<DividedMessageEmoji>(
+    (acc, emoji, currentIndex) => {
+      const currentMessage = acc[0][currentIndex];
+      const splitted = currentMessage.split(emoji);
+      const newDividedMessages = acc[0].map((m, index) => {
+        if (index !== currentIndex) return m;
+        return splitted[0];
+      });
+      return [
+        [...newDividedMessages, splitted[1]],
+        [...acc[1], emoji],
+      ];
+    },
+    [[message], []]
+  );
+
+  return separatedMessage;
+}
