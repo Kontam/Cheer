209a7cfa1a29552e9e96656c9c2d994ac7320197
Kontam <tehayan0111@gmail.com>
2023-01-02 20:56:09 +0900

    feat: enable multi byte emoji

--- app/components/atoms/Emoji/index.tsx
+++ app/components/atoms/Emoji/index.tsx
@@ -12,14 +12,23 @@ type EmojiDataSource = typeof emojiData;
 
 const Emoji: React.FC<Props> = ({ emoji, emojiExpression }) => {
   const emojiName = emojiExpression.match(':(.*):')?.[1];
-  const unicodeEmoji = (emojiData as EmojiDataSource).find(
-    (emo) => emo.short_name === emojiName
-  );
+  const unicodeEmoji: EmojiDataSource[number] | undefined = (
+    emojiData as EmojiDataSource
+  ).find((emo) => emo.short_name === emojiName);
+  const byteSplitted = unicodeEmoji?.unified.split('-') || [];
+  const unicode =
+    byteSplitted.length > 0
+      ? String.fromCodePoint(
+          ...(byteSplitted.map((byte) => {
+            return `0x${byte}`;
+          }) as any[])
+        )
+      : '';
 
   return (
     <>
-      {unicodeEmoji ? (
-        <span>{String.fromCodePoint(`0x${unicodeEmoji.unified}` as any)}</span>
+      {unicode ? (
+        <span>{unicode}</span>
       ) : (
         <img src={getEmojiSrc(emoji, emojiExpression)} alt={emojiExpression} />
       )}
