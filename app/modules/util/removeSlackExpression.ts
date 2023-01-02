3fed7f53b3a3204ee2143e1008c2e2b763b1116b
Kontam <tehayan0111@gmail.com>
2023-01-02 21:01:35 +0900

    refactor: remove unused code

--- app/modules/util/removeSlackExpression.ts
+++ app/modules/util/removeSlackExpression.ts
@@ -6,11 +6,6 @@ export function removeInlineCode(text: string) {
   return text.replace(/`/g, '');
 }
 
-// '<img width="5" height="5" src="https://emoji.slack-edge.com/TS8NZP5AQ/squirrel/465f40c0e0.png" />'
-export function removeEmojiCode(text: string) {
-  return text.replace(/:\S*?:/g, '');
-}
-
 export function removeUnusedExpression(text: string) {
   return removeInlineCode(removeMention(text));
 }
