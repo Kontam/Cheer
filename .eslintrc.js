d25810b97f6e35d3201845cd4ea753b0010f4516
Kontam <tehayan0111@gmail.com>
2023-01-07 22:53:08 +0900

    env: update eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,12 +1,18 @@
 // test
 module.exports = {
-  extends: 'erb/typescript',
+  extends: 'erb',
   rules: {
     // A temporary hack related to IDE not resolving correct package.json
     'import/no-extraneous-dependencies': 'off',
     'react/prop-types': 'off',
+    'react/require-default-props': 'off',
+    'react/jsx-filename-extension': [1, { extensions: ['.ts', '.tsx'] }],
     'jest/expect-expect': 'off',
     'import/prefer-default-export': 'off',
+    'no-use-before-define': 'off',
+    '@typescript-eslint/no-use-before-define': ['error'],
+    'no-unused-vars': 'off',
+    '@typescript-eslint/no-unused-vars': ['error'],
     '@typescript-eslint/no-explicit-any': 'off',
     '@typescript-eslint/no-unused-vars': 'off',
     '@typescript-eslint/no-empty-interface': 'off',
@@ -15,6 +21,7 @@ module.exports = {
     '@typescript-eslint/ban-ts-ignore': 'off',
     'compat/compat': 'warn',
     'react/jsx-props-no-spreading': 'off',
+    'import/extensions': 'off',
   },
   settings: {
     'import/resolver': {
@@ -24,9 +31,14 @@ module.exports = {
         config: require.resolve('./configs/webpack.config.eslint.js'),
       },
     },
+    'import/parsers': {
+      '@typescript-eslint/parser': ['.ts', '.tsx'],
+    },
   },
   parserOptions: {
+    ecmaVersion: 2020,
+    sourceType: 'module',
     project: ['./tsconfig.json'],
-    projectFolderIgnoreList: ['puppeteer']
+    projectFolderIgnoreList: ['puppeteer'],
   },
 };
