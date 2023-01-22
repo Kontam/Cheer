81a46f5877821af1e85e228f1aa7368f3937b8db
Kontam <tehayan0111@gmail.com>
2023-01-22 18:15:58 +0900

    fix: env fix storyshot errors

--- .storybook/jest.config.js
+++ .storybook/jest.config.js
@@ -1,5 +1,12 @@
 const { defaults } = require('jest-config');
 module.exports = {
+  testEnvironment: 'jsdom',
+  preset: 'ts-jest/presets/js-with-ts',
+  globals: {
+    'ts-jest': {
+      useESM: true,
+    },
+  },
   testMatch: ['**/Storyshots.test.js'],
   moduleNameMapper: {
     '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
