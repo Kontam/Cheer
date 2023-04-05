f10e15b4d14f65f583f323f6faf76c2815f24de0
Kontam <tehayan0111@gmail.com>
2023-04-05 20:01:42 +0900

    refactor: remove shell from renderer process

---
+++ app/modules/eventHandlers/main/shellEventHandlers.ts
@@ -0,0 +1,8 @@
+import { ipcMain, shell } from 'electron';
+import appConst from '../../constants/appConst';
+
+export function setShellEventHandlers() {
+  ipcMain.on(appConst.IPC_OPEN_BROWSER, (e, arg) => {
+    shell.openExternal(arg.url);
+  });
+}
