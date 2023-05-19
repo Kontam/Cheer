5c3ecaf07fc0183bf2ae4f40eceb8db20b0d02d5
Kontam <tehayan0111@gmail.com>
2023-05-20 04:21:14 +0900

    test: add invoke mock and fix test with it

--- app/lib/test/mockInvoke.ts
+++ app/lib/test/mockInvoke.ts
@@ -1,4 +1,17 @@
+import { ElectronHandler } from '../../preload';
+
+export function mockInvokeReset() {
+  (window.electron.ipcRenderer.invoke as jest.Mock).mockReset();
+}
+
 export function mockInvoke(returnValue: any) {
   (window.electron.ipcRenderer.invoke as jest.Mock).mockReset();
   window.electron.ipcRenderer.invoke = jest.fn().mockResolvedValue(returnValue);
+}
+
+export function mockInvokeWithImplementation(
+  fn: ElectronHandler['ipcRenderer']['invoke']
+) {
+  (window.electron.ipcRenderer.invoke as jest.Mock).mockReset();
+  window.electron.ipcRenderer.invoke = jest.fn(fn);
 }
